91645c2044a496a1e316233ba7496dc9b9421138
Katlego magoro <101677920+katlegomagoro@users.noreply.github.com>
2022-12-06 22:16:15 +0200

    Add files via upload

---
+++ contactus.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang = "en">
+	<head>
+		<title>Ekasi BnB Online Reservation</title>
+		<meta charset = "utf-8" />
+		<meta name = "viewport" content = "width=device-width, initial-scale=1.0" />
+		<link rel = "stylesheet" type = "text/css" href = "css/bootstrap.css " />
+		<link rel = "stylesheet" type = "text/css" href = "css/style.css" />
+	</head>
+<body>
+	<nav style = "background-color:rgba(0, 0, 0, 0.1);" class = "navbar navbar-default">
+		<div  class = "container-fluid">
+			<div class = "navbar-header">
+				<a class = "navbar-brand" >Ekasi BnB Online Reservation</a>
+			</div>
+		</div>
+	</nav>	
+	<ul id = "menu">
+		<li><a href = "index.php">Home</a></li> |
+		<li><a href = "aboutus.php">About us</a></li> |
+		<li><a href = "contactus.php">Contact us</a></li> |
+		<li><a href = "gallery.php">Gallery</a></li> |
+		<li><a href = "dineandlounge.php">Dine and Lounge</a></li> |			
+		<li><a href = "reservation.php">Make a reservation</a></li> |
+		<li><a href = "rulesandregulation.php">Rules and Regulation</a></li>
+	</ul>
+	<div style = "margin-left:0;" class = "container">
+		<div class = "panel panel-default">
+			<div class = "panel-body">
+				<strong><h3>CONTACT US</h3></strong>
+				<br />
+				<br />
+				<center><img src = "images/ekasibnb.png" width = "300" height = "300" /></center>
+				<br />
+				<br />
+				<center>
+				<p>Manger: Katlego Magoro</p>
+				<p>Contact No: +27 66 288 4270</p>
+				<p>Email: manger@ekasibnb.com</p>
+				<p>Personal Email: katlegomagagoro98@gmail.com</p>
+				<p>Head Of Staff: Athenkosi Nakhane Maqwara </p>
+				<p>Contact No: +27 79 921 8264</p>
+				<p>Email: hstaff@ekasibnb.com</p>
+				<p>Personal Email: sitamaqwara@gmail.com</p>
+				<p>Head Of Security: Chulumanco Vuyolwethu Ndela </p>
+				<p>Contact No: +27 79 921 8264</p>
+				<p>Email: hstaff@ekasibnb.com</p>
+				<p>Personal Email: sitamaqwara@gmail.com</p>
+				<p>Ekasi BnB Bookings Email: bookings@ekasibnb.com</p>
+				</center>
+			</div>
+		</div>
+	</div>
+	<br />
+	<br />
+	<div style = "text-align:right; margin-right:10px;" class = "navbar navbar-default navbar-fixed-bottom">
+		<label>&copy; Copyright HOR 2017 </label>
+	</div>
+</body>
+<script src = "js/jquery.js"></script>
+<script src = "js/bootstrap.js"></script>	
+</html>
